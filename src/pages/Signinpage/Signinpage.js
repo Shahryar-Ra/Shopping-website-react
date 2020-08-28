3dfd56a95cda295547398a312dc7cd9d5d7c9387
Shahryar <shahryar.seven@gmail.com>
2020-08-28 21:29:05 +0430

    setting up signUp component with Email and password

--- src/pages/Signinpage/Signinpage.js
+++ src/pages/Signinpage/Signinpage.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './Signinpage.scss';
 
 import SignIn from '../../components/SignIn/SignIn';
+import SignUp from '../../components/SignUp/SignUp';
 
 const Signinpage = () => {
   return (
     <div>
-      <div>
+      <div className='sign-in-and-sign-up'>
         <SignIn />
+        <SignUp />
       </div>
     </div>
   );
